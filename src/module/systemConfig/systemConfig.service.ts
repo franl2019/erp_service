e837f4f341a51fdcac0a7a77d377da66d954371b
franl2019 <1606075736@qq.com>
2022-09-18 12:30:55 +0800

    账套系统

---
+++ src/module/systemConfig/systemConfig.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class SystemConfigService {
+
+    constructor(
+
+    ) {
+    }
+
+    public async createConfig(){
+
+    }
+
+    public async updateConfig(){
+
+    }
+}
